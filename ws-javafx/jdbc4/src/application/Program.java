8f28cbcc5398d14f239ba831e79ac39f56a6260c
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-03-31 18:14:49 -0300

    MySQL - Updating Data with Java

---
+++ ws-javafx/jdbc4/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		//ATUALIZAR DADOS
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+		
+			System.out.println("Done! Rows Affected: " + rowsAffected);
+		
+		} catch (SQLException except) {
+			except.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
